6e37820ab6fae52b0a8f361f64d241f5e3b28ade
Lizhi <70121187+Lizhi-sjtu@users.noreply.github.com>
2022-05-11 20:34:28 +0800

    Add files via upload

---
+++ 4.PPO-discrete/replaybuffer.py
@@ -0,0 +1,35 @@
+import torch
+import numpy as np
+
+
+class ReplayBuffer:
+    def __init__(self, args):
+        self.s = np.zeros((args.batch_size, args.state_dim))
+        self.a = np.zeros((args.batch_size, 1))
+        self.a_logprob = np.zeros((args.batch_size, 1))
+        self.r = np.zeros((args.batch_size, 1))
+        self.s_ = np.zeros((args.batch_size, args.state_dim))
+        self.dw = np.zeros((args.batch_size, 1))
+        self.done = np.zeros((args.batch_size, 1))
+        self.count = 0
+
+    def store(self, s, a, a_logprob, r, s_, dw, done):
+        self.s[self.count] = s
+        self.a[self.count] = a
+        self.a_logprob[self.count] = a_logprob
+        self.r[self.count] = r
+        self.s_[self.count] = s_
+        self.dw[self.count] = dw
+        self.done[self.count] = done
+        self.count += 1
+
+    def numpy_to_tensor(self):
+        s = torch.tensor(self.s, dtype=torch.float)
+        a = torch.tensor(self.a, dtype=torch.long)  # In discrete action space, 'a' needs to be torch.long
+        a_logprob = torch.tensor(self.a_logprob, dtype=torch.float)
+        r = torch.tensor(self.r, dtype=torch.float)
+        s_ = torch.tensor(self.s_, dtype=torch.float)
+        dw = torch.tensor(self.dw, dtype=torch.float)
+        done = torch.tensor(self.done, dtype=torch.float)
+
+        return s, a, a_logprob, r, s_, dw, done
