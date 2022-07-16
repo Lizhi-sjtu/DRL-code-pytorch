4c5f3175e5547bc933a84cdc1304b5fb794fab14
Lizhi <70121187+Lizhi-sjtu@users.noreply.github.com>
2022-07-16 15:42:22 +0800

    Add files via upload

---
+++ 3.Rainbow_DQN/sum_tree.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+
+class SumTree(object):
+    """
+    Story data with its priority in the tree.
+    Tree structure and array storage:
+
+    Tree index:
+         0         -> storing priority sum
+        / \
+      1     2
+     / \   / \
+    3   4 5   6    -> storing priority for transitions
+
+    Array type for storing:
+    [0,1,2,3,4,5,6]
+    """
+
+    def __init__(self, buffer_capacity):
+        self.buffer_capacity = buffer_capacity  # buffer的容量
+        self.tree_capacity = 2 * buffer_capacity - 1  # sum_tree的容量
+        self.tree = np.zeros(self.tree_capacity)
+
+    def update(self, data_index, priority):
+        # data_index表示当前数据在buffer中的index
+        # tree_index表示当前数据在sum_tree中的index
+        tree_index = data_index + self.buffer_capacity - 1  # 把当前数据在buffer中的index转换为在sum_tree中的index
+        change = priority - self.tree[tree_index]  # 当前数据的priority的改变量
+        self.tree[tree_index] = priority  # 更新树的最后一层叶子节点的优先级
+        # then propagate the change through the tree
+        while tree_index != 0:  # 更新上层节点的优先级，一直传播到最顶端
+            tree_index = (tree_index - 1) // 2
+            self.tree[tree_index] += change
+
+    def get_index(self, v):
+        parent_idx = 0  # 从树的顶端开始
+        while True:
+            child_left_idx = 2 * parent_idx + 1  # 父节点下方的左右两个子节点的index
+            child_right_idx = child_left_idx + 1
+            if child_left_idx >= self.tree_capacity:  # reach bottom, end search
+                tree_index = parent_idx  # tree_index表示采样到的数据在sum_tree中的index
+                break
+            else:  # downward search, always search for a higher priority node
+                if v <= self.tree[child_left_idx]:
+                    parent_idx = child_left_idx
+                else:
+                    v -= self.tree[child_left_idx]
+                    parent_idx = child_right_idx
+
+        data_index = tree_index - self.buffer_capacity + 1  # tree_index->data_index
+        return data_index, self.tree[tree_index]  # 返回采样到的data在buffer中的index,以及相对应的priority
+
+    @property
+    def priority_sum(self):
+        return self.tree[0]  # 树的顶端保存了所有priority之和
+
+    @property
+    def priority_max(self):
+        return self.tree[self.buffer_capacity - 1:].max()  # 树的最后一层叶节点，保存的才是每个数据对应的priority
